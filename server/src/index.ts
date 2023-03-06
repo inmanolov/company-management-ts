cca8dde35cf52bed9304747f35f7fb2c0ece71fe
inmanolov <ivan.nadkov@gmail.com>
2023-03-06 10:53:43 +0200

    initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT || 5011;
+const PORT = process.env.PORT || 5011; 
 
 app.use('/employees', employeesRoutes);
 app.use('/teams', teamsRoutes);
